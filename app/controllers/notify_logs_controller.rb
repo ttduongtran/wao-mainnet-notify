59f8cd888c8dfa7671ba77b8b3c936e86ded999b
DuongTran <ttduongtran@gmail.com>
2021-04-09 17:23:49 +0700

    fxi

--- app/controllers/notify_logs_controller.rb
+++ app/controllers/notify_logs_controller.rb
@@ -1,6 +1,4 @@
 class NotifyLogsController < ApplicationController
-  has_scope :page, default: 1
-  has_scope :per, default: 10
 
   def index
     @notify_logs = NotifyLog.all.order(id: :desc).page params[:page]
