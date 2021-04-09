aeb3674069c41ae7061d78908fc91ecbc3b02bcc
DuongTran <ttduongtran@gmail.com>
2021-04-09 13:58:41 +0700

    add moniker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require rails-ujs
 //= require jquery3
+//= require activestorage
 //= require popper
 //= require bootstrap-sprockets
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root "monikers#index"
   resources :notify_logs
   resources :monikers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
