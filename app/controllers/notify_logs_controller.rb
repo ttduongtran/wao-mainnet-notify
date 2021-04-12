79dc5d14d40207f8f02e8eb5dc3cf04cde392897
DuongTran <ttduongtran@gmail.com>
2021-04-12 12:43:27 +0700

    add

--- app/controllers/notify_logs_controller.rb
+++ app/controllers/notify_logs_controller.rb
@@ -1,7 +1,23 @@
 class NotifyLogsController < ApplicationController
+  before_action :set_nofity_log, only: %i[ show edit update destroy ]
 
   def index
     @notify_logs = NotifyLog.order(id: :desc).page params[:page]
   end
+  
+  # GET /nofity_logs/1 or /nofity_logs/1.json
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_nofity_log
+      @nofity_log = NotifyLog.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def nofity_log_params
+      params.require(:nofity_log).permit(:name, :uri, :memo, :status)
+    end
 
 end
