ef87bd24bb03e7f0fa1b8d2dd9f27e31a0c71133
DuongTran <ttduongtran@gmail.com>
2021-04-13 11:07:53 +0700

    update worker

--- app/workers/check_moniker_worker.rb
+++ app/workers/check_moniker_worker.rb
@@ -5,9 +5,14 @@ class CheckMonikerWorker
   
   def perform
     begin
-      data = Moniker.all
-      msg = mainnet_perform(data, ENV["API"])
+      data_btc_eth = Moniker.find_by(moniker_type: 0)
+      msg_btc_eth = mainnet_perform(data_btc_eth, ENV["API_BTC_ETH"])
+
+      data_btc_bsc = Moniker.find_by(moniker_type: 1)
+      msg_btc_bsc = mainnet_perform(data_btc_bsc, ENV["API_BTC_BSC"])
       
+      msg = msg_btc_eth + msg_btc_bsc
+
       TelegramBot.send_msg(parse_to_message(msg)) if msg.present?
 
     rescue StandardError => e 
