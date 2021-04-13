fda7f5afe6cba268b49aac21486da3be85694ed7
DuongTran <ttduongtran@gmail.com>
2021-04-13 11:09:15 +0700

    update

--- app/workers/check_moniker_worker.rb
+++ app/workers/check_moniker_worker.rb
@@ -5,10 +5,10 @@ class CheckMonikerWorker
   
   def perform
     begin
-      data_btc_eth = Moniker.find_by(moniker_type: 0)
+      data_btc_eth = Moniker.where(moniker_type: 0)
       msg_btc_eth = mainnet_perform(data_btc_eth, ENV["API_BTC_ETH"])
 
-      data_btc_bsc = Moniker.find_by(moniker_type: 1)
+      data_btc_bsc = Moniker.where(moniker_type: 1)
       msg_btc_bsc = mainnet_perform(data_btc_bsc, ENV["API_BTC_BSC"])
       
       msg = msg_btc_eth + msg_btc_bsc
